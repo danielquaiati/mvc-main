fdc690a656183d7ca63ee58e1c04ce56dbb657c8
Quaiati <fabio.quaiati@etec.sp.gov.br>
2022-11-09 19:47:15 -0300

    inicio do projeto

---
+++ app/Model/Entity/Organization.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Model\Entity;
+
+class Organization{
+
+    public $id = 1;
+
+    public $name = 'Professor Quati';
+
+    public $site = 'Etec';
+
+    public $description = 'Nesta aulas iremos abordar o conceito de MVC e sua vantagem quanto a criação de Layout com Bootstrap';
+}
